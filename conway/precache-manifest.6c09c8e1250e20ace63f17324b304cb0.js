dfb1617978022c4b04ef6910b83be4943287fd85
rakeshbs <rakeshbs@gmail.com>
2020-04-17 18:05:10 +0530

    Add conway

---
+++ conway/precache-manifest.6c09c8e1250e20ace63f17324b304cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d1420321817a43312cf0d72feea87b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4e21478cdba9eb7dcf",
+    "url": "/static/css/main.d65f5fa8.chunk.css"
+  },
+  {
+    "revision": "a2f6ca1fab31fe35bde2",
+    "url": "/static/js/2.7d4921c1.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/static/js/2.7d4921c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4e21478cdba9eb7dcf",
+    "url": "/static/js/main.0f556655.chunk.js"
+  },
+  {
+    "revision": "a5ea60d6c88a0d34a412",
+    "url": "/static/js/runtime-main.466ccbfb.js"
+  }
+]);
